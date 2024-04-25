cc2ac7405bf52a32a2b6e1994920dc05cee66632
Oumatou <oumatoulacen@gmail.com>
2024-04-25 21:51:18 +0100

    Add index.html, package.json, and src/index.js files for Webpack setup

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+
+// Append paragraphs to the body
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
