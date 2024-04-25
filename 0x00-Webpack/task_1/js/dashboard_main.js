c1e099ddf40418041887a3437aec402850ea5b0f
Oumatou <oumatoulacen@gmail.com>
2024-04-26 00:46:04 +0100

    Refactor dashboard_main.js to improve code readability and fix variable name typo

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,24 +1,17 @@
 import $ from 'jquery';
-import _ from 'lodash';  // Importing Lodash
+import _ from 'lodash';
 
-// Adding the required elements to the page
-$('body').append('<p>Holberton Dashboard</p>');  // First paragraph
-$('body').append('<p>Dashboard data for the students</p>');  // Second paragraph
-$('body').append('<button id="start-button">Click here to get started</button>');  // Button
-$('body').append("<p id='count'></p>");  // Paragraph with ID 'count'
-$('body').append('<p>Copyright - Holberton School</p>');  // Final paragraph
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
-// Variable to track the button click count
-let clickCount = 0;
+let conunt = 0;
 
-// Function to update the click count
 function updateCounter() {
-  clickCount++;
-  $('#count').text(`${clickCount} clicks on the button`);
-}
+  count++;
+  $("#count").html(`${count} clicks on the button`);
+};
 
-// Using Lodash's debounce function with a delay of 300ms
-const debouncedUpdateCounter = _.debounce(updateCounter, 300);
-
-// Bind the debounced function to the button's click event
-$('#start-button').on('click', debouncedUpdateCounter);
+$('button').on('click', _.debounce(updateCounter, 500));
