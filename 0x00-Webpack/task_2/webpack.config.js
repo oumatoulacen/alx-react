2d0101a3cbf0b83f4ad6583404c7661bbb321562
Oumatou <oumatoulacen@gmail.com>
2024-04-26 13:32:01 +0100

    Update Webpack configuration to set mode to production and change output directory to 'public'

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    main: path.resolve(__dirname, "./js/dashboard_main.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  performance: {
+    // disable performance hints (warnings and errors)
+    hints: false,
+    // these limits provide guidance on acceptable sizes for Webpack assets and entry points.
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(gif|png|jp?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
+
+
+
+
+
