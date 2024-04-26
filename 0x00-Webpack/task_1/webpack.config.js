59d75ee9ca4f2df85f4e39f65b94a155963094bf
Oumatou <oumatoulacen@gmail.com>
2024-04-26 01:03:10 +0100

    Update Webpack configuration to set mode to production and change output directory to 'public'

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production',  // Set mode to production
   entry: './js/dashboard_main.js',  // Entry point
   output: {
     filename: 'bundle.js',  // Output filename
-    path: path.resolve(__dirname, 'dist'),  // Output directory
+    path: path.resolve(__dirname, 'public'),  // Output directory
   },
 };
