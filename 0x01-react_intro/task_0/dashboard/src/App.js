934b3ea4548240578f1cccb27b55bb68c345ec5e
Oumatou <oumatoulacen@gmail.com>
2024-04-27 04:31:09 +0100

    Basic application

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,1 +1,19 @@
-#!/usr/bin/node
+import './App.css';
+import logo from './holberton_logo.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt='logo' />
+        <h1>School dashboard</h1>
+      </header>
+      <body className='App-body'>
+        <p>Login to access the full dashboard</p>
+      </body>
+      <footer className='App-footer'>Copyright 2020 - holberton School</footer>
+    </div>
+  );
+}
+
+export default App;
