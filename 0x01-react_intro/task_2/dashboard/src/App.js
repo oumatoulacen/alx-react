e20b0b4f0ca84c2ae81ce6d8363942131f48830a
Oumatou <oumatoulacen@gmail.com>
2024-04-27 06:46:58 +0100

    Modify the App

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -1,1 +1,25 @@
-#!/usr/bin/node
+import './App.css';
+import logo from './holberton_logo.jpg';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt='logo' />
+        <h1>School dashboard</h1>
+      </header>
+      <body className='App-body'>
+        <p>Login to access the full dashboard</p>
+        <label for='email'>Email</label>
+        <input id='email' type='email' />
+        <label for='password'>Password</label>
+        <input id='password' type='password' />
+        <button>OK</button>
+      </body>
+      <footer className='App-footer'>Copyright {getFullYear()} - {getFooterCopy(true)}</footer>
+    </div>
+  );
+}
+
+export default App;
--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+const getFullYear = () => new Date().getFullYear();
+
+const getFooterCopy = (isIndex) => {
+    return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
+}
+
+export { getFullYear, getFooterCopy };
