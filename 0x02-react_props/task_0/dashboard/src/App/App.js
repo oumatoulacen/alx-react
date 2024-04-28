1de839ddb1f6ae0fa88072f8ebe5aec103d1fa7f
Oumatou <oumatoulacen@gmail.com>
2024-04-28 21:02:29 +0100

    Basic components

--- 0x02-react_props/task_0/dashboard/src/App/App.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -1,30 +1,20 @@
 import React from "react";
-import logo from "../assets/holberton-logo.jpg";
 import "./App.css";
-import { getFullYear, getFooterCopy } from "../utils/utils";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>School dashboard</h1>
-      </div>
+      <Header />
       <div className="App-body">
-        <p>Login to access the full dashboard</p>
-        <form>
-          <label htmlFor="email">Email:</label>
-          <input type="email" name="email"></input>
-          <label htmlFor="password">Password:</label>
-          <input type="password" name="password"></input>
-          <button>OK</button>
-        </form>
+          <Login />
       </div>
-      <div className="App-footer">
-        Copyright {getFullYear()} - {getFooterCopy()}
-      </div>
+      <Footer />
     </div>
   );
 }
 
 export default App;
+ 
