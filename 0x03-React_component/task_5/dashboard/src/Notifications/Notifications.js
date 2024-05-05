07a59560e77426c21afa51d4869430ab1af9d98a
Oumatou <oumatoulacen@gmail.com>
2024-05-05 05:30:21 +0100

    Make your own pure component

--- 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.js
+++ 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.js
@@ -12,6 +12,12 @@ class Notifications extends Component {
     this.markAsRead = this.markAsRead.bind(this);
   }
 
+  // Only re-render if the new listNotifications has more elements than before
+  shouldComponentUpdate(nextProps) {
+    return nextProps.listNotifications.length > this.props.listNotifications.length;
+  }
+  
+
   markAsRead(id) {
     console.log(`Notification ${id} has been marked as read`);
   }
