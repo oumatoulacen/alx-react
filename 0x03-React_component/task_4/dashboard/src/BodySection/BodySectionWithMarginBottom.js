5f9216c5bbc34add1ddfaa83943a99ea5f3f6063
Oumatou <oumatoulacen@gmail.com>
2024-05-05 01:38:59 +0100

    Create WithLogging HOC

---
+++ 0x03-React_component/task_4/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import BodySection from './BodySection';
+import './BodySectionWithMarginBottom.css';
+
+class BodySectionWithMarginBottom extends Component {
+    render() {
+        return (
+        <div className="bodySectionWithMargin">
+            <BodySection {...this.props} />
+        </div>
+        );
+    }
+}
+
+BodySectionWithMarginBottom.propTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.element
+    ])
+  };
+
+BodySectionWithMarginBottom.defaultProps = {
+    children: <React.Fragment />
+};
+
+export default BodySectionWithMarginBottom;
