43c3a12c3b2ec077b7db8d4d61a05bf8209e071f
Oumatou <oumatoulacen@gmail.com>
2024-05-11 12:37:30 +0100

    Lazy Seq

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,1 +1,18 @@
-#!/usr/bin/node
+import { Seq } from 'immutable';
+
+const printBestStudents = (students) => {
+  const bestStudents = Seq(students)
+    .filter((student) => student.score >= 70)
+    .map((student) => {
+      const { firstName, lastName } = student;
+      return {
+        ...student,
+        firstName: firstName.charAt(0).toUpperCase() + firstName.slice(1),
+        lastName: lastName.charAt(0).toUpperCase() + lastName.slice(1),
+      };
+    });
+
+  console.log(bestStudents.toJS());
+};
+
+export default printBestStudents;
--- 0x06-React_Immutable/test.js
+++ 0x06-React_Immutable/test.js
@@ -75,21 +75,37 @@
 // It accepts two arguments map1 and map2. Both are Immutable.js Maps
 // It should return true if the Maps are equal
 // Example:
-import areMapsEqual from './7-equality.js';
+// import areMapsEqual from './7-equality.js';
 
-const map1 = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132,
+// const map1 = {
+//   fear: true,
+//   smell: -1033575916.9145899,
+//   wall: false,
+//   thing: -914767132,
+// };
+
+// const map2 = {
+//   fear: true,
+//   smell: -1033575916.9145899,
+//   wall: false,
+//   thing: -914767132,
+// };
+
+// const res = areMapsEqual(map1, map2);
+// console.log('task7: res:', res);
+
+// console.log("-------------------------------------------------");
+
+// 8. Lazy Seq
+
+import printBestStudents from './8-seq.js';
+
+const grades = {
+  1: {
+    score: 99,
+    firstName: 'guillaume',
+    lastName: 'salva',
+  },
 };
 
-const map2 = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132,
-};
-
-const res = areMapsEqual(map1, map2);
-console.log('task7: res:', res);
+printBestStudents(grades);
