9bde9f5d551514038132c9810accb51c65f5d1bc
Oumatou <oumatoulacen@gmail.com>
2024-05-11 12:24:55 +0100

    Equality

--- 0x06-React_Immutable/7-equality.js
+++ 0x06-React_Immutable/7-equality.js
@@ -1,1 +1,4 @@
-#!/usr/bin/node
+import { is, Map } from 'immutable';
+
+const areMapsEqual = (map1, map2) => is(Map(map1), Map(map2));
+export default areMapsEqual;
--- 0x06-React_Immutable/test.js
+++ 0x06-React_Immutable/test.js
@@ -40,29 +40,56 @@
 // console.log("-------------------------------------------------");
 
 // 6. Nested merge
-import mergeDeeplyElements from './6-deeply.js';
+// import mergeDeeplyElements from './6-deeply.js';
 
-const page1 = {
-  'user-1': {
-    id: 1,
-    name: 'test',
-    likes: {
-      1: {
-        uid: 1234,
-      },
-    },
-  },
+// const page1 = {
+//   'user-1': {
+//     id: 1,
+//     name: 'test',
+//     likes: {
+//       1: {
+//         uid: 1234,
+//       },
+//     },
+//   },
+// };
+
+// const page2 = {
+//   'user-1': {
+//     likes: {
+//       2: {
+//         uid: 134,
+//       },
+//     },
+//   },
+// };
+
+// const res = mergeDeeplyElements(page1, page2).toJS();
+// console.log('task6: res:', res);
+
+// console.log("-------------------------------------------------");
+
+// 7. Equality
+// test a function named areMapsEqual
+
+// It accepts two arguments map1 and map2. Both are Immutable.js Maps
+// It should return true if the Maps are equal
+// Example:
+import areMapsEqual from './7-equality.js';
+
+const map1 = {
+  fear: true,
+  smell: -1033575916.9145899,
+  wall: false,
+  thing: -914767132,
 };
 
-const page2 = {
-  'user-1': {
-    likes: {
-      2: {
-        uid: 134,
-      },
-    },
-  },
+const map2 = {
+  fear: true,
+  smell: -1033575916.9145899,
+  wall: false,
+  thing: -914767132,
 };
 
-const res = mergeDeeplyElements(page1, page2).toJS();
-console.log('task6: res:', res);
+const res = areMapsEqual(map1, map2);
+console.log('task7: res:', res);
