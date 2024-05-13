6fb672f460f7476f13650a0e8f63512ac48f170e
Oumatou <oumatoulacen@gmail.com>
2024-05-13 18:56:40 +0100

    Create actions for the UI

---
+++ 0x07-react_redux_action_creator_normalizr/task_4/dashboard/src/actions/courseActionCreators.js
@@ -0,0 +1,17 @@
+import { SELECT_COURSE, UNSELECT_COURSE } from './courseActionTypes';
+
+const selectCourse = (index) => {
+    return {
+        type: SELECT_COURSE,
+        index
+    };
+    };
+
+const unSelectCourse = (index) => {
+    return {
+        type: UNSELECT_COURSE,
+        index
+    };
+}
+
+export { selectCourse, unSelectCourse };
