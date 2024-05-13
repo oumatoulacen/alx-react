c52f8966f13937c3e44cae02024f54781c06e58a
Oumatou <oumatoulacen@gmail.com>
2024-05-13 20:17:53 +0100

    Bound the actions

--- 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/actions/notificationActionCreators.js
+++ 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/actions/notificationActionCreators.js
@@ -1,17 +1,29 @@
-import { MARK_AS_READ, SET_TYPE_FILTER, NotificationTypeFilters } from "./notificationActionTypes";
+import { MARK_AS_READ, SET_TYPE_FILTER, NotificationTypeFilters } from "../../../../task_5/dashboard/src/actions/notificationActionTypes";
 
-const markAsAread = (index) => {
+const markAsRead = (index) => {
     return {
         type: MARK_AS_READ,
         index
     };
-}
+};
 
 const setNotificationFilter = (filter) => {
     return {
         type: SET_TYPE_FILTER,
         filter
     };
-}
+};
 
-export { markAsAread, setNotificationFilter };
+const boundMarkAsRead = (index) => {
+    return (dispatch) => {
+        dispatch(markAsRead(index));
+    };
+};
+
+const boundSetNotificationFilter = (filter) => {
+    return (dispatch) => {
+        dispatch(setNotificationFilter(filter));
+    };
+};
+
+export { markAsRead, setNotificationFilter, boundMarkAsRead, boundSetNotificationFilter };
--- 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/actions/uiActionCreators.js
+++ 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/actions/uiActionCreators.js
@@ -1,28 +1,52 @@
-import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from "./uiActionTypes";
+import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from "../../../../task_5/dashboard/src/actions/uiActionTypes";
 
 const login = (email, password) => {
   return {
     type: LOGIN,
     user: { email, password }
   };
-}
+};
 
 const logout = () => {
     return {
         type: LOGOUT
     };
-}
+};
 
 const displayNotificationDrawer = () => {
     return {
         type: DISPLAY_NOTIFICATION_DRAWER
     };
-}
+};
 
 const hideNotificationDrawer = () => {
     return {
         type: HIDE_NOTIFICATION_DRAWER
     };
-}
+};
 
-export { login, logout, displayNotificationDrawer, hideNotificationDrawer };
+const boundLogin = (email, password) => {
+  return (dispatch) => {
+    dispatch(login(email, password));
+  };
+};
+
+const boundLogout = () => {
+  return (dispatch) => {
+    dispatch(logout());
+  };
+};
+
+const boundDisplayNotificationDrawer = () => {
+  return (dispatch) => {
+    dispatch(displayNotificationDrawer());
+  };
+};
+
+const boundHideNotificationDrawer = () => {
+  return (dispatch) => {
+    dispatch(hideNotificationDrawer());
+  };
+};
+
+export { login, logout, displayNotificationDrawer, hideNotificationDrawer, boundLogin, boundLogout, boundDisplayNotificationDrawer, boundHideNotificationDrawer };
