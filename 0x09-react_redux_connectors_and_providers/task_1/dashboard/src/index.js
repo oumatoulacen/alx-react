dd1f09a1303c838154d1c4485be5a4e3ff9caf2f
Oumatou <oumatoulacen@gmail.com>
2024-05-16 13:27:19 +0100

    Update mapStateToProps

---
+++ 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App/App";
+import { createStore } from "redux";
+import { Map } from "immutable";
+import { Provider } from "react-redux";
+import uiReducer, { initialState } from "./reducers/uiReducer";
+
+// const store = createStore(uiReducer, Map(initialState));
+const store = createStore(uiReducer);
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
