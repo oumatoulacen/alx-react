e9b466c6be8152d9b3f3cb35ca8515220b05bca6
Oumatou <oumatoulacen@gmail.com>
2024-05-17 04:10:49 +0100

    Understand how to use the Redux Chrome extension

--- 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -1,20 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import App from "./App/App";
+import uiReducer, { initialState } from "./reducers/uiReducer";
 import { Map } from "immutable";
 
-// redux
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import uiReducer, { initialState } from "./reducers/uiReducer";
-import thunk from "redux-thunk";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// const store = createStore(
+//   uiReducer,
+//   Map(initialState),
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
 
-// components
-import App from "./App/App";
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-
-// const store = createStore(uiReducer, Map(initialState));
-const store = createStore(uiReducer, initialState, applyMiddleware(thunk));
-
+const store = createStore(
+  uiReducer,
+  Map(initialState),
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <React.StrictMode>
